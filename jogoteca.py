851450ce4a7167870ba91aeb641c465896b0c1fc
daniel <heromon.9010@gmail.com>
2022-10-09 14:55:32 -0300

    concertando meus erros

--- jogoteca.py
+++ jogoteca.py
@@ -1,18 +1,15 @@
-from crypt import methods
 from flask import Flask, render_template, request, redirect
-from dataclasses import dataclass
 
-@dataclass
 class Jogo:
-    nome: str
-    categoria: str
-    console: str
+    def __init__(self, nome, categoria, console):
+        self.nome=nome
+        self.categoria=categoria
+        self.console=console
 
 jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
 jogo2 = Jogo('God of War', 'Hack n Slash', 'PS2')
 jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
 lista = [jogo1, jogo2, jogo3]
-
 
 app = Flask(__name__)
 
@@ -20,14 +17,17 @@ app = Flask(__name__)
 def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
-app.route('/criar', methods=['POST',])
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
 def criar():
     nome = request.form['nome']
-    categoria = request['categoria']
+    categoria = request.form['categoria']
     console = request.form['console']
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
     return redirect('/')
 
 app.run(debug=True)
-
