7e836283655b4ff486f7c991acecc2d7523aec03
Daniel <heromon.9010@gmail.com>
2022-10-13 22:27:12 -0300

    criando condicional que redireciona pra novas páginas

--- jogoteca.py
+++ jogoteca.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template, request, redirect, session, flash
+
+
 class Jogo:
     def __init__(self, nome, categoria, console):
-        self.nome=nome
-        self.categoria=categoria
-        self.console=console
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
 
 jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
 jogo2 = Jogo('God of War', 'Hack n Slash', 'PS2')
@@ -13,15 +16,20 @@ lista = [jogo1, jogo2, jogo3]
 app = Flask(__name__)
 app.secret_key = 'Miguel'
 
+
 @app.route('/')
 def index():
     return render_template('lista.html', titulo='Jogos', jogos=lista)
 
+
 @app.route('/novo')
 def novo():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login')
     return render_template('novo.html', titulo='Novo Jogo')
 
-@app.route('/criar', methods=['POST',])
+
+@app.route('/criar', methods=['POST', ])
 def criar():
     nome = request.form['nome']
     categoria = request.form['categoria']
@@ -29,10 +37,14 @@ def criar():
     jogo = Jogo(nome, categoria, console)
     lista.append(jogo)
     return redirect('/')
+
+
 @app.route('/login')
 def login():
     return render_template('login.html')
-@app.route('/autenticar', methods=['POST',])
+
+
+@app.route('/autenticar', methods=['POST', ])
 def autenticar():
     if 'key' == request.form['senha']:
         session['usuario_logado'] = request.form['usuario']
@@ -40,10 +52,13 @@ def autenticar():
         return redirect('/')
     else:
         return redirect('/login')
+
+
 @app.route('/logout')
 def logout():
     session['usuario_logado'] = None
     flash('Logout efetuado com sucesso!')
     return redirect('/')
+
 
 app.run(debug=True)
